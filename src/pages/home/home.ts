370fd3ab2abdf01fd0f863312e7315e866653d7e
gomescgcpereira <gomescgconsultoria@gmail.com>
2018-02-03 16:52:16 -0200

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import * as HighCharts from 'highcharts'
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad(){
+    var myChart = HighCharts.chart('container', {
+      chart: {
+      type: 'bar'
+      },
+      title: {
+      text: 'Fruit Consumption'
+      },
+      xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+      title: {
+      text: 'Fruit eaten'
+      }
+      },
+      series: [{
+      name: 'Jane',
+      data: [1, 0, 4]
+      }, {
+      name: 'John',
+      data: [5, 7, 3]
+      }]
+      });
+  }
+}
